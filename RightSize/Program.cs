c46623306ca48d3f08323f47494b7ed414392489
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-05 14:48:58 +0000

    Add project RightSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+           foreach (string s in args)
+           {
+              if(s.Length > 3 && s.Length < 8)
+              {
+                 Console.WriteLine(s);
+              }
+              else if(s.Length > 8)
+              {
+                Console.WriteLine("[EARLY STOP]");
+                break;
+              }
+           }
+        }
+    }
+}
