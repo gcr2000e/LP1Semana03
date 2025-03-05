28dc86c1d61e8cfbc7b6cabf9eedfe0b9f2be781
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-05 17:35:03 +0000

    Add ChangeString program

---
+++ ChangeString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string s = Console.ReadLine();
+
+            Console.Write("Caráter: ");
+            char c = Console.ReadKey().KeyChar;
+
+            char[] chars = s.ToCharArray();
+
+            for(int i = 0; i < chars.Length; i++)
+            {
+               if (chars[i] == c)
+               {
+                   chars[i] = 'x';
+               }
+            }
+
+            string StringModificada = new string(chars);
+            Console.WriteLine("\nSerá impresso: " + StringModificada);
+        }
+    }
+}
